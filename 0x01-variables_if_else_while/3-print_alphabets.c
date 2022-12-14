eb4a89205bf6f6686d153c13dd491abbf847cd91
BIhezande1990 <uwase2014@gmail.com>
2022-12-14 06:08:49 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main -  print ch in lowercase and uppercase
+ * description - ch followed by new line \n
+ * return: always 0 (succes)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for( ch = a; ch <= 'z'; ch++)
+		puchat(ch);
+	for( ch = A; ch < 'Z'; ch++)
+		putchar(ch);
+	puchar(\n);
+		return(0);
+
