86401ac2ec97301971a86b74f0a953b38d5f2397
Bihezande1990 <uwase2014@gmail.com>
2022-12-15 01:55:04 -0800

    lower except q,a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print an alphabet in lower case
+ * description - except 'e' or 'q' followed by new line \n
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if(ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
