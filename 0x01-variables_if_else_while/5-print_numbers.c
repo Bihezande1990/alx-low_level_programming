513078de221c638943d20604a3717e2d7a9f684c
Bihezande1990 <uwase2014@gmail.com>
2022-12-15 05:45:42 -0800

    round posi

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ * 
+ * from 0 to 10 followed by new line \n
+ *
+ * return: aways 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n=0; n<9; n++)
+		printf("%d" digit is, n);
+	printf("\n");
+	return (0);
+}
