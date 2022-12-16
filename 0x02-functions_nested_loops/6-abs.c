6703a2cea5e406114a1bff38c554c07cba31e170
Bihezande1990 <uwase2014@gmail.com>
2022-12-16 06:49:48 -0800

    print sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute integer
+ * @n: integer to be computed
+ *
+ * Return: the absolute value of integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n)
+	else
+		return (-n)
+}
