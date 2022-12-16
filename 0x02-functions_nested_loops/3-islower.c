714973c1624e4417dfdb9ca68e04266e85315003
Bihezande1990 <uwase2014@gmail.com>
2022-12-16 02:23:43 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,10 @@ include "main.h"
  * Return: 0
  */
 int _islower(int c)
+
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
+	if (c >="a" && c <="z")
+	return (1);
 	else
-		return (0);
+	return (0);
 }
