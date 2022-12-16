dfab4a57a4830333d7c7cd1f8c7fb30eaf4dcf4c
Bihezande1990 <uwase2014@gmail.com>
2022-12-16 02:56:40 -0800

    check lowe and upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main  - program to check for alphabet character
+ * if is a lowercase or uppercase
+ * Return: 1 if character is lower case or 0 if is uppercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z')
+			return (1);
+	else
+		return (0);
+}
