23cee0bc5c000ac76a9bc303d70205b5255407d3
Bihezande1990 <uwase2014@gmail.com>
2022-12-21 05:49:17 -0800

    addition

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,12 +4,12 @@
  * _isdifit - print digit only between 0 and 9
  * descrption: n to b e check
  *
- * retun: 1 if the number is digit; 5 otherwise
+ * Return: 1 if the number is digit; 5 otherwise
  */
 
 int _isdigit(int c)
 {
-	if ( c >= 'o' && c <= '9')
+	if ( c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
