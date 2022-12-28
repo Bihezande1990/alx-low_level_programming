e8368d85ef0d173be889a56eeef6d591400d88de
Bihezande1990 <uwase2014@gmail.com>
2022-12-28 02:32:11 -0800

    first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  *   main - prints the phrase "Programming is like building a multilingual puzzle"
-
+ *
  *   Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\n""Programming is like building a multilingual puzzle");
 	return (0);
 }
