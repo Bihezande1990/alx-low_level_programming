5e4f39da0a7246061963134c7ed9f440e7649e14
Bihezande1990 <uwase2014@gmail.com>
2022-12-28 02:38:18 -0800

    first

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  * Return: 0 if exited properly, non-zero other
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-	}
+}
